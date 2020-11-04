cefcd9866b45663ad435933d3c06405f2b5f8ced
dongzhonghua03 <dongzhonghua03@kuaishou.com>
2020-11-04 22:08:32 +0800

    fix

--- reload-test/src/main/java/dsvshx/loader/MyClassLoader.java
+++ reload-test/src/main/java/dsvshx/loader/MyClassLoader.java
@@ -12,55 +12,47 @@ import java.util.Objects;
  */
 public class MyClassLoader extends ClassLoader {
     public String rootPath;
+    public String[] classPaths;
     public List<String> clazzs;
 
     public MyClassLoader(String rootPath, String... classPaths) throws Exception {
         System.out.println("============myclassloader:" + MyClassLoader.class.getClassLoader());
         this.rootPath = rootPath;
         clazzs = new ArrayList<>();
+        this.classPaths = classPaths;
         // 扫描并加载类，打破双拼委派模型
         for (String classPath : classPaths) {
-            loadClassPath(new File(classPath));
-            // this.loadClass(classPath);
+            this.findClass(classPath);
         }
-    }
-
-    public void loadClassPath(File file) throws Exception {
-        if (file.isDirectory()) {
-            for (File f : Objects.requireNonNull(file.listFiles())) {
-                loadClassPath(f);
-            }
-        } else {
-            String fileName = file.getName();
-            String filePath = file.getPath();
-            String endName = fileName.substring(fileName.lastIndexOf(".") + 1);
-            if (endName.equals("class")) {
-                FileInputStream fileInputStream = new FileInputStream(file);
-                byte[] bytes = new byte[(int) file.length()];
-                fileInputStream.read(bytes);
-                String className = filePathToClassName(filePath);
-                clazzs.add(className);
-                // class文件已经到了虚拟机了
-                defineClass(className, bytes, 0, bytes.length);
-            }
-        }
-    }
-
-    private String filePathToClassName(String filePath) {
-        String className = filePath.replace(rootPath, "").replaceAll("/", ".");
-        return className.substring(1, className.lastIndexOf("."));
     }
 
     @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException {
-        // TODO: 这里需要加锁
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
         Class<?> clazz = findLoadedClass(name);
         if (clazz == null) {
-            if (!clazzs.contains(name)) {
-                System.out.println(name);
-                return super.loadClass(name);
+            File file = new File(name);
+            if (file.isDirectory()) {
+                for (File f : Objects.requireNonNull(file.listFiles())) {
+                    this.findClass(f.getPath());
+                }
             } else {
-                throw new ClassNotFoundException("加载不到类！");
+                try {
+                    String fileName = file.getName();
+                    String filePath = file.getPath();
+                    String endName = fileName.substring(fileName.lastIndexOf(".") + 1);
+                    if (endName.equals("class")) {
+                        FileInputStream fileInputStream = new FileInputStream(file);
+                        byte[] bytes = new byte[(int) file.length()];
+                        fileInputStream.read(bytes);
+                        String className = filePath.replace(rootPath, "").replaceAll("/", ".");
+                        className = className.substring(1, className.lastIndexOf("."));
+                        clazzs.add(className);
+                        // class文件已经到了虚拟机了
+                        return defineClass(className, bytes, 0, bytes.length);
+                    }
+                } catch (Exception e) {
+                    return super.findClass(name);
+                }
             }
         }
         return clazz;
